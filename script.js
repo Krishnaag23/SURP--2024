d2a3b0fe89414f7193c13ee9c530205027aa91dd
Cezan Vispi Damania <cezandam15625@gmail.com>
2024-07-15 20:28:18 +0530

    Added Apply Now button to modal

--- script.js
+++ script.js
@@ -111,6 +111,7 @@ fetch('projects.json')
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary apply-now-btn">Apply Now</button>
                         </div>
                     </div>
                 </div>
